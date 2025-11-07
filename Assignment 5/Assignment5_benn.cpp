a2b8e01371a12546df58a479ff42385bff073334
benn <192007>
2025-11-06 22:55:21 -0800

    finished

--- Assignment 5/Assignment5_benn.cpp
+++ Assignment 5/Assignment5_benn.cpp
@@ -1,6 +1,9 @@
-//
-// Created by Benn on 11/6/25.
-//
+// Assignment 5
+// This program calculate users' idea body weight in pounds.
+// Expected input in "names-1.txt" file as followed:
+// Name (Ex: John Haha)
+// Height in Feet (Ex: 6)
+// Extra Inches (Ex: 5)
 
 #include <iostream>
 #include <fstream>
@@ -8,39 +11,49 @@
 using namespace std;
 
 int main() {
-    string name, line;
-    int i,j, feet, inches, count = 0;
-
+    string name;
+    string temp_feet, temp_inches;
 
     ifstream file;
-    file.open("num1.txt");
-    //read how many total lines
-    if (file) {
-        cout << "open sucessfully!";
-        // while (getline(file, line)) {
-        //     count++;
-        // }
-        // cout << "There are: " << count << "lines";
-        // //Create a for loop interval for I number of lines
-        // for (i = 0; i < count; i++) {
-        //     //Create an inner loop for total of 3 times to read and assign names, feet, inches
-        //     cout << "Hello";
-        //
-        //
-        //
-        // }
-        file.close();
-        //Create an if statement to calculate if feet > 5.
-        //If not, output cant calculate.
-
-
+    file.open("names-1.txt");
+    //check if the file can be opened or not
+    if (!file) {
+        cout << "Check again! File can't be opened!\n";
     }
-    else {
-        cout << "Failed to open file!";
+    //This infinity loop will be executed until there is no line left
+    while (true) {
+        //take in value for the first line of name, if there is none -> break
+        if (!getline(file, name)) break;
+        //take in value for the second line of feet value, if there is none -> break
+        if (!getline(file, temp_feet)) break;
+        //take in value for the third line of inches, if there is none -> break
+        if (!getline(file, temp_inches)) break;
+
+        //Conversion from the string of the files to integers
+        int feet = stoi(temp_feet);
+        int inches = stoi(temp_inches);
+        //Just used to check the types of these integers
+            // cout << feet << " and " << inches << endl;
+            // cout << typeid(feet).name() << endl;
+            // cout << typeid(inches).name() << endl;
+
+        //Main Calculation Of Idea Body Weight
+        if (feet >= 5) {
+            int idea_body_weight = 110 + ((feet - 5) * 12 + inches) * 5;
+            cout << "The idea body weight for " << name << " is " << idea_body_weight << " pounds. \n";
+        }
+        else {
+            cout << "I can't calculate the ideal body weight for " << name << "." << endl;
+        }
     }
-
-
-
-
+    file.close();
     return 0;
 }
+/*
+Sample Run:
+The idea body weight for Tom Atto is 185 pounds.
+The idea body weight for Eaton Wright is 135 pounds.
+The idea body weight for Cary Oki is 165 pounds.
+I can't calculate the ideal body weight for Omar Ahmed.
+
+*/
