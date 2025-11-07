6ca355be889db3bc60dca0e442bc6b8921c421ea
benn <192007>
2025-11-06 20:57:28 -0800

    checking for validity of file

--- Assignment 5/Assignment5_benn.cpp
+++ Assignment 5/Assignment5_benn.cpp
@@ -2,7 +2,6 @@
 // Created by Benn on 11/6/25.
 //
 
-#include "Assignment5_benn.h"
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -14,20 +13,25 @@ int main() {
 
 
     ifstream file;
-    file.open("num-1.txt");
+    file.open("num1.txt");
     //read how many total lines
     if (file) {
-        while (getline(file, line)) {
-            count++;
-        }
-        cout << "There are: " << count << "lines";
-    //Create a for loop interval for i number of lines
-    for (i = 0; i < count; i++) {
-        //Create an inner loop for total of 3 times to read and assign names, feet, inches
-        cout << "Hello";
-    }
-    //Create an if statement to calculate if feet > 5.
-    //If not, output cant calculate.
+        cout << "open sucessfully!";
+        // while (getline(file, line)) {
+        //     count++;
+        // }
+        // cout << "There are: " << count << "lines";
+        // //Create a for loop interval for I number of lines
+        // for (i = 0; i < count; i++) {
+        //     //Create an inner loop for total of 3 times to read and assign names, feet, inches
+        //     cout << "Hello";
+        //
+        //
+        //
+        // }
+        file.close();
+        //Create an if statement to calculate if feet > 5.
+        //If not, output cant calculate.
 
 
     }
