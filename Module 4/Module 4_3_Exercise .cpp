e5fed60912cb26cbb91d0c07d982ac8d070ac6dc
benn <192007>
2025-11-08 18:09:02 -0800

    Finished product

---
+++ Module 4/Module 4_3_Exercise .cpp
@@ -0,0 +1,45 @@
+//BennNguyen_Exercise4.3
+//8th Nov 2025
+/*Finish the following program
+
+‘A’ category (90–100),
+‘B’ category (80–89),
+“You Pass” category (60–79),
+“You Fail” category (0–59).
+"invalid" otherwise */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float average; // holds the grade average
+
+    cout << "Input your average:" << endl;
+    cin >> average;
+    if( average >= 90.0 && average <= 100.0 )
+        cout << 'A' << endl;
+    else if( average >= 80.0 && average <= 89.0 )
+        cout << 'B' << endl;
+    else if( average >= 60 && average <= 79)
+        cout << "You Pass" << endl;
+    else if( average >= 0 && average <= 59)
+        cout << "You Failed" << endl;
+    else cout << "Invalid" << endl;
+
+    return 0;
+}
+/*sample run1
+Input your average:
+85
+B */
+/*sample run2
+Input your average:
+30
+You Fail */
+/*sample run3
+Input your average:
+-12
+Invalid */
+
+
