a7de84ec82e77c015986124dc30da91a192e6d07
benn <192007>
2025-11-10 06:43:59 -0800

    Input integer

---
+++ Module 5/Module_5_3_Exercise.cpp
@@ -0,0 +1,27 @@
+//BennNguyen_Exercise5.2
+//9th Nov 2025
+/*Write a program that asks the user for a positive integer no greater than 15.
+ *Then it displays character 'X' as many time as the user input.
+ *you must use for loop.
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int pos_num,count_x, i;
+
+    cout << "Enter a positive integer less than 15: "; cin >> pos_num;
+    for (pos_num != 15);
+        cout << "Enter a positive integer less than 15: "; cin >> pos_num;
+
+
+    for (i = 0; i < 15; i++) {
+        cout << "Enter a positive integer less than 15: ";
+        cin >> pos_num;
+    }
+
+
+
+    return 0;
+}
